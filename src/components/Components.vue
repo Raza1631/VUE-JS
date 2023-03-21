9cf5a83589313d5668af38f79c9cbb363409dbce
Raza1631 <razaahmed163101@gmail.com>
2023-03-22 00:09:55 +0500

    basic cocept clear

---
+++ src/components/Components.vue
@@ -0,0 +1,43 @@
+<template>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Name : {{firstName+" "+lastName}}</h1>
+    <h2>Email :{{email}}</h2>
+    <h2>Email :{{email=="razahmed163101@gmail.com"}}</h2>
+    <h3>Age :{{getAge(21)}}</h3>
+    <h4>Points {{points}}</h4>
+    <button v-on:click="pluspoints()">Plus</button>
+</template>
+
+<script>
+export default {
+    name:"BasicComponenets",
+    data(){
+        return {
+            firstName: "Raza",
+            lastName: "Ahmed",
+            email: "razahmed163101@gmail.com",
+            
+            points: 0
+        }
+    },
+    methods:{
+        getAge:function(age){
+            return age
+        },
+        pluspoints:function(){
+            this.points+=this.getAge(20);
+        }
+    }
+}
+</script>
+
+<style>
+#app{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Components from './components/Components'
 
-createApp(App).mount('#app')
+createApp(Components).mount('#app')
